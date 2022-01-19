e040d37f9adbf5295dceb2a16710d9417fdaf12a
Jéssica <84422472+Bentojessica@users.noreply.github.com>
2022-01-19 14:23:46 -0300

    Challenge-EventStore finalizado

---
+++ src/mainTS/calculateEmployeeSalary.ts
@@ -0,0 +1,44 @@
+import {
+    validateEmptyProperties,
+    ValidateEmptyPropertiesOutput,
+  } from "./validateEmptyProperties";
+  
+  
+  export interface CalculateEmployeeSalaryInput {
+    employeeName: string;
+    baseSalary: number;
+    benefits: number[];
+    extraHours: number;
+  }
+  
+  export const calculateEmployeeSalary = (
+    input: CalculateEmployeeSalaryInput,
+    validator: (input: any) => ValidateEmptyPropertiesOutput
+  ) => {
+    const validationResult: ValidateEmptyPropertiesOutput = validator(input);
+  
+    if (!validationResult.isValid) {
+      throw new Error("Missing Properties");
+    }
+  
+    if (input.baseSalary < 0) {
+      throw new Error("Invalid BaseSalary");
+    }
+  
+    let fullSalary = input.baseSalary;
+  
+    for (const benefit of input.benefits) {
+      if (benefit < 0) {
+        throw new Error("Invalid Benefit");
+      }
+      fullSalary += benefit;
+    }
+  
+    if (input.extraHours < 0) {
+      throw new Error("Invalid ExtraHours");
+    }
+  
+    fullSalary += input.extraHours;
+  
+    return fullSalary;
+  };
